ba56e1ee81f52e0a661997c575428d509face726
Aurelius Wendelken <webtobesocial@gmail.com>
2020-12-13 22:27:35 +0100

    added query search lookup

--- asgi.py
+++ asgi.py
@@ -7,7 +7,7 @@ import yfinance as yf
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from pymongo import ASCENDING
+from pymongo import ASCENDING, TEXT
 from pymongo.errors import DuplicateKeyError
 
 from datetime import date, datetime, timedelta
@@ -19,14 +19,10 @@ def is_nan(v):
 
 def connect_mongodb():
     mongo_uri = 'mongodb://localhost:27017'
-
     client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
-
     db = client.stockdata
 
-    col = db.get_collection('data')
-
-    return col
+    return db
 
 
 def download_dataset(symbol, start, end):
@@ -35,16 +31,16 @@ def download_dataset(symbol, start, end):
     return d
 
 
-async def insert_dataframes(d, s):
+async def insert_dataframes(db, d, s):
     a = yf.Ticker(s)
 
     for i in d.iterrows():
         try:
-            await col.insert_one({'timestamp': i[0], 'open': i[1][0],
-                                  'high': i[1][1], 'low': i[1][2],
-                                  'close': i[1][3], 'volume': i[1][5],
-                                  'adjust_close': i[1][4], 'symbol': s,
-                                  'isin': a.isin})
+            await db['data'].insert_one({'timestamp': i[0], 'open': i[1][0],
+                                         'high': i[1][1], 'low': i[1][2],
+                                         'close': i[1][3], 'volume': i[1][5],
+                                         'adjust_close': i[1][4], 'symbol': s,
+                                         'isin': a.isin})
 
             print(s, i[0], i[1][0], i[1][1], i[1]
                   [2], i[1][3], i[1][4], i[1][5])
@@ -104,13 +100,26 @@ async def read_symbols(col):
     return res
 
 
-app = FastAPI()
-col = connect_mongodb()
+async def lookup_query(col, q):
+    res = await col.aggregate([{'$match': {'$text': {'$search': q}}},
+                               {'$project': {'_id': 0, 'symbol': 1, 'isin': 1,
+                                             'long_name': 1, 'score':
+                                                 {'$meta': 'textScore'}}}
+                               ]).to_list(length=10000)
 
-col.create_index(
+    return res
+
+
+app = FastAPI()
+db = connect_mongodb()
+
+db['data'].create_index(
     [('symbol', ASCENDING), ('timestamp', ASCENDING)], unique=True
 )
 
+db['info'].create_index(
+    [('long_name', TEXT), ('symbol', TEXT), ('isin', TEXT)], name='query_index'
+)
 
 origins = [
     'http://localhost:3000'
@@ -125,10 +134,16 @@ app.add_middleware(
 )
 
 
+@app.get('/query/{query}')
+async def list_results(query):
+    results = await lookup_query(db['info'], query)
+
+    return {'results': [r for r in results]}
+
+
 @app.get('/symbols')
 async def list_all_symbols():
-
-    symbols = await read_symbols(col)
+    symbols = await read_symbols(db['info'])
 
     return {'symbols': [s for s in symbols]}
 
@@ -138,7 +153,7 @@ async def average_values(symbol):
     object = {}
 
     symbol = symbol.upper()
-    symbols = await read_symbols(col)
+    symbols = await read_symbols(db['info'])
 
     if symbol not in [s for s in symbols]:
         return {}, 404
@@ -146,50 +161,51 @@ async def average_values(symbol):
     # fetch latest indicies
     # await request_download(symbol)
 
-    high_one_day = await read_average(col, symbol, 'high', 1)
+    high_one_day = await read_average(db['data'], symbol, 'high', 1)
     object['high_one_day'] = high_one_day
 
-    high_ten_days = await read_average(col, symbol, 'high', 10)
+    high_ten_days = await read_average(db['data'], symbol, 'high', 10)
     object['high_ten_days'] = high_ten_days
 
-    high_thirty_days = await read_average(col, symbol, 'high', 30)
+    high_thirty_days = await read_average(db['data'], symbol, 'high', 30)
     object['high_thirty_days'] = high_thirty_days
 
-    low_one_day = await read_average(col, symbol, 'low', 1)
+    low_one_day = await read_average(db['data'], symbol, 'low', 1)
     object['low_one_day'] = low_one_day
 
-    low_ten_days = await read_average(col, symbol, 'low', 10)
+    low_ten_days = await read_average(db['data'], symbol, 'low', 10)
     object['low_ten_days'] = low_ten_days
 
-    low_thirty_days = await read_average(col, symbol, 'low', 30)
+    low_thirty_days = await read_average(db['data'], symbol, 'low', 30)
     object['low_thirty_days'] = low_thirty_days
 
-    open_one_day = await read_average(col, symbol, 'open', 1)
+    open_one_day = await read_average(db['data'], symbol, 'open', 1)
     object['open_one_day'] = open_one_day
 
-    open_ten_days = await read_average(col, symbol, 'open', 10)
+    open_ten_days = await read_average(db['data'], symbol, 'open', 10)
     object['open_ten_days'] = open_ten_days
 
-    open_thirty_days = await read_average(col, symbol, 'open', 30)
+    open_thirty_days = await read_average(db['data'], symbol, 'open', 30)
     object['open_thirty_days'] = open_thirty_days
 
-    close_one_day = await read_average(col, symbol, 'close', 1)
+    close_one_day = await read_average(db['data'], symbol, 'close', 1)
     object['close_one_day'] = close_one_day
 
-    close_ten_days = await read_average(col, symbol, 'close', 10)
+    close_ten_days = await read_average(db['data'], symbol, 'close', 10)
     object['close_ten_days'] = close_ten_days
 
-    close_thirty_days = await read_average(col, symbol, 'close', 30)
+    close_thirty_days = await read_average(db['data'], symbol, 'close', 30)
     object['close_thirty_days'] = close_thirty_days
 
-    adjust_one_day = await read_average(col, symbol, 'adjust_close', 1)
+    adjust_one_day = await read_average(db['data'], symbol, 'adjust_close', 1)
     object['adjust_one_day'] = adjust_one_day
 
-    adjust_ten_days = await read_average(col, symbol, 'adjust_close', 10)
+    adjust_ten_days = await read_average(
+        db['data'], symbol, 'adjust_close', 10)
     object['adjust_ten_days'] = adjust_ten_days
 
     adjust_thirty_days = await read_average(
-        col, symbol, 'adjust_close', 30)
+        db['data'], symbol, 'adjust_close', 30)
     object['adjust_thirty_days'] = adjust_thirty_days
 
     return {'averages': [object]}
