fe2db488da6c63718a241f70f93d9935fe78f5dd
Aurelius Wendelken <webtobesocial@gmail.com>
2020-12-15 10:46:58 +0100

    updated webserver to handle forex usd to eur list in averages

--- asgi.py
+++ asgi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import math
+import pytz
 import motor.motor_asyncio
 import yfinance as yf
 
@@ -8,8 +9,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from pymongo import ASCENDING, TEXT
-from pymongo.errors import DuplicateKeyError
-
 from datetime import date, datetime, timedelta
 
 
@@ -31,60 +30,26 @@ def download_dataset(symbol, start, end):
     return d
 
 
-async def insert_dataframes(db, d, s):
-    a = yf.Ticker(s)
-
-    for i in d.iterrows():
-        try:
-            await db['data'].insert_one({'timestamp': i[0], 'open': i[1][0],
-                                         'high': i[1][1], 'low': i[1][2],
-                                         'close': i[1][3], 'volume': i[1][5],
-                                         'adjust_close': i[1][4], 'symbol': s,
-                                         'isin': a.isin})
-
-            print(s, i[0], i[1][0], i[1][1], i[1]
-                  [2], i[1][3], i[1][4], i[1][5])
-        except DuplicateKeyError:
-            print(f'ERROR: duplicate key {i[0]} with symbol {s}')
-
-
-async def request_download(symbol):
-    factor_start = 0
-    factor_end = 7
-
-    delta_end = timedelta(days=factor_end)
-    initial_end_date = date.today().isoformat()
-    end_iso_date = datetime.fromisoformat(initial_end_date) - delta_end
-
-    delta_start = timedelta(days=factor_start)
-    start_iso_date = datetime.fromisoformat(initial_end_date) - delta_start
-
-    end_date = start_iso_date.strftime('%Y-%m-%d')
-    start_date = end_iso_date.strftime('%Y-%m-%d')
-
-    print(start_date, end_date)
-
-    data = download_dataset(symbol, start_date, end_date)
-    await insert_dataframes(data, symbol)
-
-
-async def read_average(col, symbol, field, days):
+async def read_average(db, symbol, field, days):
     data = {}
-    now = datetime.today()
-    weekday_now = now.weekday()
+    naive_datetime = datetime.combine(date.today(), datetime.max.time())
+    timezone = pytz.timezone('US/Eastern')
+    now = timezone.localize(naive_datetime)
+    weekday = now.weekday()
 
-    if weekday_now == 5:
+    if weekday == 5:
         now = now - timedelta(days=1)
-    elif weekday_now == 6:
+    elif weekday == 6:
         now = now - timedelta(days=2)
 
     end = now - timedelta(days=days)
 
-    res = await col.aggregate([{'$match': {'symbol': symbol, 'timestamp':
-                                           {'$lt': now, '$gte': end}}},
-                               {'$group': {'_id': '$symbol', 'average':
-                                           {'$avg': f'${field}'}}},
-                               {'$limit': 1}]).to_list(length=1)
+    res = await db['data'].aggregate([{'$match': {'symbol': symbol,
+                                                  'timestamp': {'$lt': now,
+                                                                '$gte': end}}},
+                                      {'$group': {'_id': '$symbol', 'average':
+                                                  {'$avg': f'${field}'}}},
+                                      {'$limit': 1}]).to_list(length=1)
 
     if res and res[0]['average'] is not None and not is_nan(res[0]['average']):
         data['value'] = str(round(res[0]['average'], 2))
@@ -94,18 +59,29 @@ async def read_average(col, symbol, field, days):
     return data
 
 
-async def read_symbols(col):
-    res = await col.distinct('symbol')
+async def read_symbols(db):
+    res = await db['info'].distinct('symbol')
 
     return res
 
 
-async def lookup_query(col, q):
-    res = await col.aggregate([{'$match': {'$text': {'$search': q}}},
-                               {'$project': {'_id': 0, 'symbol': 1, 'isin': 1,
-                                             'long_name': 1, 'score':
-                                                 {'$meta': 'textScore'}}}
-                               ]).to_list(length=10000)
+async def read_info(db, symbol):
+    res = await db['info'].find({'symbol': symbol})
+
+    return res
+
+
+async def lookup_query(db, q):
+    res = await db['data'].aggregate([{'$match': {'$text': {'$search': q}}},
+                                      {'$project': {'_id': 0, 'symbol': 1,
+                                                    'isin': 1, 'long_name': 1}}
+                                      ]).to_list(length=10000)
+
+    return res
+
+
+async def retrieve_info(db, symbol):
+    res = await db['info'].find({'symbol': symbol}).to_list(length=10000)
 
     return res
 
@@ -136,14 +112,14 @@ app.add_middleware(
 
 @app.get('/query/{query}')
 async def list_results(query):
-    results = await lookup_query(db['info'], query)
+    results = await lookup_query(db, query)
 
     return {'results': [r for r in results]}
 
 
 @app.get('/symbols')
 async def list_all_symbols():
-    symbols = await read_symbols(db['info'])
+    symbols = await read_symbols(db)
 
     return {'symbols': [s for s in symbols]}
 
@@ -153,59 +129,59 @@ async def average_values(symbol):
     object = {}
 
     symbol = symbol.upper()
-    symbols = await read_symbols(db['info'])
+    metadata = await read_info(db, symbol)
+    symbols = await read_symbols(db)
 
     if symbol not in [s for s in symbols]:
-        return {}, 404
+        return {}
 
-    # fetch latest indicies
-    # await request_download(symbol)
+    high_two_days = await read_average(db, symbol, 'high_eur', 2)
+    object['high_two_days'] = high_two_days
 
-    high_one_day = await read_average(db['data'], symbol, 'high', 1)
-    object['high_one_day'] = high_one_day
-
-    high_ten_days = await read_average(db['data'], symbol, 'high', 10)
+    high_ten_days = await read_average(db, symbol, 'high_eur', 10)
     object['high_ten_days'] = high_ten_days
 
-    high_thirty_days = await read_average(db['data'], symbol, 'high', 30)
+    high_thirty_days = await read_average(db, symbol, 'high_eur', 30)
     object['high_thirty_days'] = high_thirty_days
 
-    low_one_day = await read_average(db['data'], symbol, 'low', 1)
-    object['low_one_day'] = low_one_day
+    low_two_days = await read_average(db, symbol, 'low_eur', 2)
+    object['low_two_days'] = low_two_days
 
-    low_ten_days = await read_average(db['data'], symbol, 'low', 10)
+    low_ten_days = await read_average(db, symbol, 'low_eur', 10)
     object['low_ten_days'] = low_ten_days
 
-    low_thirty_days = await read_average(db['data'], symbol, 'low', 30)
+    low_thirty_days = await read_average(db, symbol, 'low_eur', 30)
     object['low_thirty_days'] = low_thirty_days
 
-    open_one_day = await read_average(db['data'], symbol, 'open', 1)
-    object['open_one_day'] = open_one_day
+    open_two_days = await read_average(db, symbol, 'open_eur', 2)
+    object['open_two_days'] = open_two_days
 
-    open_ten_days = await read_average(db['data'], symbol, 'open', 10)
+    open_ten_days = await read_average(db, symbol, 'open_eur', 10)
     object['open_ten_days'] = open_ten_days
 
-    open_thirty_days = await read_average(db['data'], symbol, 'open', 30)
+    open_thirty_days = await read_average(db, symbol, 'open_eur', 30)
     object['open_thirty_days'] = open_thirty_days
 
-    close_one_day = await read_average(db['data'], symbol, 'close', 1)
-    object['close_one_day'] = close_one_day
+    close_two_days = await read_average(db, symbol, 'close_eur', 2)
+    object['close_two_days'] = close_two_days
 
-    close_ten_days = await read_average(db['data'], symbol, 'close', 10)
+    close_ten_days = await read_average(db, symbol, 'close_eur', 10)
     object['close_ten_days'] = close_ten_days
 
-    close_thirty_days = await read_average(db['data'], symbol, 'close', 30)
+    close_thirty_days = await read_average(db, symbol, 'close_eur', 30)
     object['close_thirty_days'] = close_thirty_days
 
-    adjust_one_day = await read_average(db['data'], symbol, 'adjust_close', 1)
-    object['adjust_one_day'] = adjust_one_day
+    adjust_two_days = await read_average(
+        db, symbol, 'adjust_close_eur', 2)
+    object['adjust_two_days'] = adjust_two_days
 
     adjust_ten_days = await read_average(
-        db['data'], symbol, 'adjust_close', 10)
+        db, symbol, 'adjust_close_eur', 10)
     object['adjust_ten_days'] = adjust_ten_days
 
     adjust_thirty_days = await read_average(
-        db['data'], symbol, 'adjust_close', 30)
+        db, symbol, 'adjust_close_eur', 30)
     object['adjust_thirty_days'] = adjust_thirty_days
 
-    return {'averages': [object]}
+    return {'averages': [object], 'isin': metadata['isin'],
+            'long_name': metadata['long_name']}
