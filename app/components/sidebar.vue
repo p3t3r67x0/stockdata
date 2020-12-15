6682d11a806c7d47b75f2fda89280f2653a2ec0c
Aurelius Wendelken <webtobesocial@gmail.com>
2020-12-15 10:45:11 +0100

    updated some vue files in very early stage

--- app/components/sidebar.vue
+++ app/components/sidebar.vue
@@ -2,88 +2,80 @@
 <div v-click-outside="hideNav" class="sm:flex sm:min-h-screen h-12 sm:h-auto">
   <div class="fixed w-full top-0 left-0 sm:w-64 overflow-y-auto transition duration-300 transform ease-in overflow-y-auto sm:translate-x-0 sm:static sm:inset-0 z-50 sm:z-0 bg-gray-900">
     <div class="flex justify-between items-center bg-gray-900 sm:border-b-4 sm:border-gray-900 h-12 sm:h-16 lg:h-20">
-      <div>
-        <img src="/150.png" class="w-48 sm:w-full sm:object-cover px-2 sm:px-4">
-      </div>
-
-      <client-only>
-        <div @click="toggleNav" class="block sm:hidden focus:outline-none overflow-hidden cursor-pointer h-12 w-12 pr-3">
-          <img src="/150.png" class="h-full w-full object-cover" />
-        </div>
-      </client-only>
+      <p class="text-white text-3xl px-2 sm:px-4">stocklify.dev</p>
     </div>
 
     <ul :class="[showNav ? 'block z-50' : 'hidden']" class="sm:block sm:mt-8">
       <li @click="toggleNav" :class="[comparePageName('dashboard') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/dashbrd">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'chart-line']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Dashboard</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('organization') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/organation">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'columns']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Organisation</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('poll') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none  sm:mt-1 py-3 px-3 sm:px-6" to="/poll">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none  sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'poll']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Umfragen</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('templates') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/temples">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'mail-bulk']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Vorlagen</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('calendar') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/calenr">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'calendar-week']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Kalender</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('language') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/langue">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'font']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Sprache</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('settings') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 sm:focus:outline-none mt-1 py-3 px-3 sm:px-6" to="/settis">
+        <nuxt-link class="grid grid-cols-6 sm:focus:outline-none mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'user-cog']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Profile</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('users') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/users">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'users']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Nutzer</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('boats') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/boats">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'ship']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Boote</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('theme') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/theme">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'paint-brush']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Theme</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" :class="[comparePageName('event') ? activeClass : inactiveClass]" class="text-white">
-        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/event">
+        <nuxt-link class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6" to="/">
           <fa :icon="['fas', 'sticky-note']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Events</span>
+          <span class="col-span-5">Menu entry</span>
         </nuxt-link>
       </li>
       <li @click="toggleNav" class="sm:hidden text-white border-gray-900 text-white hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100">
         <a class="grid grid-cols-6 focus:outline-none sm:mt-1 py-3 px-3 sm:px-6">
           <fa :icon="['fas', 'sign-out-alt']" class="col-span-1 mt-1" />
-          <span class="col-span-5">Logout</span>
+          <span class="col-span-5">Menu entry</span>
         </a>
       </li>
     </ul>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,22 +8,22 @@
       <th class="w-1/6 text-left px-3 py-1">Average</th>
     </tr>
     <tr class="bg-gray-100 text-lg">
-      <td class="font-bold p-3">High 1 day</td>
-      <td class="p-3">{{ avgData['averages'][0]['high_one_day']['end'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['high_one_day']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['high_one_day']['value'] }} USD</td>
+      <td class="font-bold p-3">High 2 days</td>
+      <td class="p-3">{{ avgData['averages'][0]['high_two_days']['end'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['high_two_days']['start'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['high_two_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-300 text-lg">
       <td class="font-bold p-3">High 10 days</td>
       <td class="p-3">{{ avgData['averages'][0]['high_ten_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['high_ten_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['high_ten_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['high_ten_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-100 text-lg">
       <td class="font-bold p-3">High 30 days</td>
       <td class="p-3">{{ avgData['averages'][0]['high_thirty_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['high_thirty_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['high_thirty_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['high_thirty_days']['value'] }} EUR</td>
     </tr>
   </table>
   <table v-if="'averages' in avgData && avgData.averages.length > 0" class="table-fixed w-full mb-6">
@@ -34,22 +34,22 @@
       <th class="w-1/6 text-left px-3 py-1">Average</th>
     </tr>
     <tr class="bg-gray-100 text-lg">
-      <td class="font-bold p-3">Low 1 day</td>
-      <td class="p-3">{{ avgData['averages'][0]['low_one_day']['end'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['low_one_day']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['low_one_day']['value'] }} USD</td>
+      <td class="font-bold p-3">Low 2 days</td>
+      <td class="p-3">{{ avgData['averages'][0]['low_two_days']['end'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['low_two_days']['start'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['low_two_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-300 text-lg">
       <td class="font-bold p-3">Low 10 days</td>
       <td class="p-3">{{ avgData['averages'][0]['low_ten_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['low_ten_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['low_ten_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['low_ten_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-100 text-lg">
       <td class="font-bold p-3">Low 30 days</td>
       <td class="p-3">{{ avgData['averages'][0]['low_thirty_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['low_thirty_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['low_thirty_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['low_thirty_days']['value'] }} EUR</td>
     </tr>
   </table>
   <table v-if="'averages' in avgData && avgData.averages.length > 0" class="table-fixed w-full mb-6">
@@ -60,22 +60,22 @@
       <th class="w-1/6 text-left px-3 py-1">Average</th>
     </tr>
     <tr class="bg-gray-300 text-lg">
-      <td class="font-bold p-3">Open 1 day</td>
-      <td class="p-3">{{ avgData['averages'][0]['open_one_day']['end'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['open_one_day']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['open_one_day']['value'] }} USD</td>
+      <td class="font-bold p-3">Open 2 days</td>
+      <td class="p-3">{{ avgData['averages'][0]['open_two_days']['end'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['open_two_days']['start'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['open_two_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-100 text-lg">
       <td class="font-bold p-3">Open 10 days</td>
       <td class="p-3">{{ avgData['averages'][0]['open_ten_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['open_ten_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['open_ten_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['open_ten_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-300 text-lg">
       <td class="font-bold p-3">Open 30 days</td>
       <td class="p-3">{{ avgData['averages'][0]['open_thirty_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['open_thirty_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['open_thirty_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['open_thirty_days']['value'] }} EUR</td>
     </tr>
   </table>
   <table v-if="'averages' in avgData && avgData.averages.length > 0" class="table-fixed w-full mb-6">
@@ -86,22 +86,22 @@
       <th class="w-1/6 text-left px-3 py-1">Average</th>
     </tr>
     <tr class="bg-gray-100 text-lg">
-      <td class="font-bold p-3">Close 1 day</td>
-      <td class="p-3">{{ avgData['averages'][0]['close_one_day']['end'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['close_one_day']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['close_one_day']['value'] }} USD</td>
+      <td class="font-bold p-3">Close 2 days</td>
+      <td class="p-3">{{ avgData['averages'][0]['close_two_days']['end'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['close_two_days']['start'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['close_two_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-300 text-lg">
       <td class="font-bold p-3">Close 10 days</td>
       <td class="p-3">{{ avgData['averages'][0]['close_ten_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['close_ten_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['close_ten_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['close_ten_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-100 text-lg">
       <td class="font-bold p-3">Close 30 days</td>
       <td class="p-3">{{ avgData['averages'][0]['close_thirty_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['close_thirty_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['close_thirty_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['close_thirty_days']['value'] }} EUR</td>
     </tr>
   </table>
   <table v-if="'averages' in avgData && avgData.averages.length > 0" class="table-fixed w-full mb-6">
@@ -112,22 +112,22 @@
       <th class="w-1/6 text-left px-3 py-1">Average</th>
     </tr>
     <tr class="bg-gray-300 text-lg">
-      <td class="font-bold p-3">Adjust close 1 day</td>
-      <td class="p-3">{{ avgData['averages'][0]['adjust_one_day']['end'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['adjust_one_day']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['adjust_one_day']['value'] }} USD</td>
+      <td class="font-bold p-3">Adjust close 2 days</td>
+      <td class="p-3">{{ avgData['averages'][0]['adjust_two_days']['end'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['adjust_two_days']['start'] }}</td>
+      <td class="p-3">{{ avgData['averages'][0]['adjust_two_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-100 text-lg">
       <td class="font-bold p-3">Adjust close 10 days</td>
       <td class="p-3">{{ avgData['averages'][0]['adjust_ten_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['adjust_ten_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['adjust_ten_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['adjust_ten_days']['value'] }} EUR</td>
     </tr>
     <tr class="bg-gray-300 text-lg">
       <td class="font-bold p-3">Adjust close 30 days</td>
       <td class="p-3">{{ avgData['averages'][0]['adjust_thirty_days']['end'] }}</td>
       <td class="p-3">{{ avgData['averages'][0]['adjust_thirty_days']['start'] }}</td>
-      <td class="p-3">{{ avgData['averages'][0]['adjust_thirty_days']['value'] }} USD</td>
+      <td class="p-3">{{ avgData['averages'][0]['adjust_thirty_days']['value'] }} EUR</td>
     </tr>
   </table>
   <div class="gf-card__chart">
@@ -228,7 +228,7 @@ export default {
   created() {
     this.maxValue = max(data, d => d.high).toFixed(2)
 
-    this.$axios.$get('http://localhost:8000/average/fb').then(res => {
+    this.$axios.$get('http://localhost:8000/average/amzn').then(res => {
       this.avgData = res
     })
   }
