069bb74eb38bbfe0b0c351d8b107b6844c652457
Aurelius Wendelken <webtobesocial@gmail.com>
2020-12-28 12:42:54 +0100

    fixed table display settings

--- app/pages/symbols.vue
+++ app/pages/symbols.vue
@@ -5,12 +5,12 @@
   </h1>
   <table v-if="infos.length > 0" class="table-fixed w-full">
     <tr class="bg-gray-700 text-white text-md">
-      <th colspan="2" class="text-left p-3">Symbol</th>
+      <th colspan="1" class="text-left p-3">Symbol</th>
       <th colspan="2" class="text-left p-3">Company</th>
       <th colspan="1" class="text-left p-3">ISIN</th>
     </tr>
     <tr v-for="info in infos" class="even:bg-gray-400 odd:bg-gray-200">
-      <td colspan="2" class="p-3">
+      <td colspan="1" class="p-3">
         <nuxt-link :to="makeLink(info['_id'])" class="text-xl sm:text-2xl block">
           <span class="bg-blue-500 font-sans text-white px-1">{{ info['_id'] }}</span>
         </nuxt-link>
@@ -19,7 +19,7 @@
         <nuxt-link :to="makeLink(info['_id'])" class="text-xl block">{{ info['long_name'] }}</nuxt-link>
       </td>
       </td>
-      <td colspan="2" class="p-3">
+      <td colspan="1" class="p-3">
         <nuxt-link :to="makeLink(info['_id'])" class="text-xl block">{{ info['isin'] }}</nuxt-link>
       </td>
     </tr>
